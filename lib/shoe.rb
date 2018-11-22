b12d7a171ad80670a3442951f6575ae0d029f458
Susan Isserman <bluenote@outlook.com>
2018-11-22 17:37:57 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
@@ -15,5 +16,6 @@ class Shoe
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
-
+  
 end
+binding.pry
